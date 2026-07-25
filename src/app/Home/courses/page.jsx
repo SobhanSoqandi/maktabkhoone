0747aeb766f23c949e239418db4ffb14c25bdfda
SobhanSoqandi <sobhansoqandi@gmail.com>
2026-07-25 11:15:35 +0330

    courses list

--- src/app/Home/courses/page.jsx
+++ src/app/Home/courses/page.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React from 'react'
+import Toolbar from './(Toolbar)/Toolbar'
+import CourseList from './(CourseList)/CourseList'
+
+import { courses } from "../../(courses)/course-data";
+import CoursesHeader from './(HeaderCourses)/CoursesHeader';
+
 
 function page() {
-  return <div> </div>;
+  return (
+    <div className="container mx-auto md:px-3 mt-5">
+
+      <div className="grid grid-cols-12 gap-8">
+
+        <div className="col-span-4 xl:col-span-3" >
+          <Toolbar />
+        </div>
+
+        <div className="col-span-12 lg:col-span-8 xl:col-span-9 ">
+
+          <CoursesHeader />
+
+          {/* <CoursesList /> */}
+          <div>
+            <CourseList courses={courses} />
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  )
 }
 
-export default page;
+export default page
